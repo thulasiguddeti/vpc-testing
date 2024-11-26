aff308e073adcd524441806b9a3649f8a1a8ad74
thulasiguddeti <thulasiguddeti02@gmail.com>
2024-11-26 15:01:03 +0530

    vpc-testing

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "roboshop" {
-  source = "../terraform-aws-vpc"
+  #source = "../terraform-aws-vpc"
+  source = "git::https://github.com/thulasiguddeti/terraform-aws-vpc.git?ref=main"
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
